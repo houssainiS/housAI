6d0cb444db28be2111debbb63d2afe8e45ab1f65
houssainiS <houssaini.slimen@gmail.com>
2025-04-21 14:40:08 +0100

    removed the / in getresponse url & removed logout from work

--- work/urls.py
+++ work/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
 from . import views
 app_name = 'work' 
 
 urlpatterns = [
     path('<int:user_id>/', views.home, name='home'),  # Accept user_id in the URL
-    path('logout/', LogoutView.as_view(next_page='login:index'), name='logout'),  # Logout view
     path('<int:user_id>/advancedMode/', views.advanced_mode , name='advanced_mode'),
-    path('<int:user_id>/advancedMode/getResponse/',views.get_ai_response, name='get_ai_response'),
+    path('<int:user_id>/advancedMode/getResponse',views.get_ai_response, name='get_ai_response'),
     path('save/', views.save_generated_website, name='save_generated_website'),
     path('<int:user_id>/history/', views.history, name='history'),
     path('<int:user_id>/history/<int:website_id>/', views.view_saved_website, name='view_saved_website'),
